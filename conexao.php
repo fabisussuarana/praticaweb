dab75f04feccdaa5c145195d6974a52e06265a9a
fabisussuarana <106176566+fabisussuarana@users.noreply.github.com>
2022-06-08 08:03:46 -0500

    criei o arquivo conexao

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+
+$servidor = "localhost";
+$bdname = "cadastro";
+$usuario = "root";
+$senha = "";
+$porta = 3306;
+
+$conexao = msqli_connect($servidor, $usuario, $senha, $bdname);
+
+if(!conexao)
+    die("Problemas com a conexão com o banco de dados. Descriçãop do problema: " . mysqli_connect_error());
+
+echo "Conexão realizada com sucesso!";
+
+?>
