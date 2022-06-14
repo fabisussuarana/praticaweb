185fcba37b3b7723b37b49ddd26dd503becdfb66
fabisussuarana <106176566+fabisussuarana@users.noreply.github.com>
2022-06-14 12:13:23 -0500

    fiz alterações

--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,11 @@
 <?php
 
-    include(conexao.php);
+    include("./conexao.php");
 
+    $nome = $_POST['nome'];
+    $data = $_POST['data'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = 
 ?>
