4754f8a32e1ff307ceb860f11db9d5f555856ebd
fabisussuarana <106176566+fabisussuarana@users.noreply.github.com>
2022-06-14 11:53:35 -0500

    Fiz alterações

--- conexao.php
+++ conexao.php
@@ -9,7 +9,7 @@ $porta = 3306;
 $conexao = mysqli_connect($servidor, $usuario, $senha, $bdname);
 
 if(!$conexao)
-    die("Problemas com a conexão com o banco de dados. Descriçãop do problema: " . mysqli_connect_error());
+    die("Problemas com a conexão com o banco de dados. Descrição do problema: " . mysqli_connect_error());
 
 echo "Conexão realizada com sucesso!";
 
